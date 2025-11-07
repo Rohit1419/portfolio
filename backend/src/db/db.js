bb77d5049fee67ea57b7df80f1432a7f65a38941
Rohit Gite <113468823+rohit-g-03@users.noreply.github.com>
2025-11-08 01:23:31 +0530

    setting up the backend for portfolio

---
+++ backend/src/db/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+DB_Name = "portfolioDB";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log(`MongoDb connected : ${mongoose.connection.host}`);
+  } catch (error) {
+    console.log("Error connecting to database:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import app from "./app.js";
+
+dotenv.config({ path: "./.env" });
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error:", error);
+    });
+
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 5000}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
